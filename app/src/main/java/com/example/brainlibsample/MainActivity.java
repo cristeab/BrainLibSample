8e502afc2d069a4bd9ab1a0f5e01baeed353fd54
Bogdan Cristea <cristeab@gmail.com>
2020-06-18 15:21:43 +0300

    add external libs

--- app/src/main/java/com/example/brainlibsample/MainActivity.java
+++ app/src/main/java/com/example/brainlibsample/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.brain.core.Md5UtilWrapper;
+
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("brain_wrapper");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test calls
+        Md5UtilWrapper.computeMD5("test");
     }
 }
